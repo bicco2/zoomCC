bdbfadeff9425c55591aab292a8aafa2a501650d
jinhobyeong <jhb_0921@naver.com>
2021-12-29 16:25:38 +0900

    2 commit

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,2 @@
-alert("hi");
+const socket = new WebSocket(`ws://${window.location.host}`);
+// 여기 소켓은 서버로의 련결을 뜻함
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -9,5 +11,18 @@ app.get("/",(req,res)=> res.render("home"));
 app.get("/*",(req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
 
+const server = http.createServer(app); //이건 http 서버임 
+//웹소켓을 위해 꼭 필요한 부분 
+const wss = new WebSocket.Server({ server });
+//이건 웹 소켓인데 http 위에 만들어진 웹소켓임 
+
+
+
+function handleConnection(socket){ //여기서의 소켓은 연결된 브라우저를 뜻함 
+    console.log(socket);
+}
+
+wss.on("connection", handleConnection)
+
+server.listen(3000, handleListen);
